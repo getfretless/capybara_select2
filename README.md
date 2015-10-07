2244fed77e3c8ba88fa6617214e331badf09e203
Miles Z. Sterrett <miles@mileszs.com>
2015-10-07 10:58:42 -0400

    Upgrade to work with select 4.0

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Inpsired by [goodwill's capybara-select2](https://github.com/goodwill/capybara-select2), this gem assists in testing forms that uses the [select2](http://ivaynberg.github.io/select2/) select box replacement.
 
-To be frank, the other gem was out of date, and no fork had everything we needed. They were also very ... _procedural_.
+(We needed the gem to work with MiniTest.)
 
 ## Installation
 
--- lib/capybara_select2.rb
+++ lib/capybara_select2.rb
@@ -3,59 +3,79 @@ require "capybara_select2/version"
 module Capybara
   module Select2
     class Element
-      attr_accessor :page, :search_enabled, :multiselect, :container
+      attr_accessor :page, :options, :search_enabled, :multiselect, :container, :drop_container
 
       def initialize(page_context, options = {})
         self.page = page_context
+        self.options= options
         self.search_enabled = !!options[:search]
         self.multiselect = !!options[:multiple]
         find_container(options.slice(:from, :xpath))
       end
 
-      def drop_container_class
-        if search_enabled then '.select2-results' else '.select2-drop' end
+      def drop_container_class(value)
+        if options.has_key? :search
+          page.find(:xpath, "//body").find(".select2-search input.select2-search__field").set(value)
+          page.execute_script(%|$("input.select2-search__field:visible").keyup();|)
+          ".select2-results"
+        elsif page.find(:xpath, "//body").has_selector?(".select2-dropdown")
+          # select2 version 4.0
+          ".select2-dropdown"
+        else
+          ".select2-drop"
+        end
       end
 
-      def select_match!
-        page.find(:css, drop_container_class).find(:css, ".select2-match").click
+      def select_match!(value)
+        [value].flatten.each do |value|
+          if page.find(:xpath, "//body").has_selector?("#{drop_container_class(value)} li.select2-results__option")
+            # select2 version 4.0
+            page.find(:xpath, "//body").find("#{drop_container_class(value)} li.select2-results__option", text: value).click
+          else
+            page.find(:xpath, "//body").find("#{drop_container_class(value)} li.select2-result-selectable", text: value).click
+          end
+        end
       end
 
-      def initiate!(value)
+      def initiate!
         focus!
-        search!(value) if search_enabled
       end
 
       private
       def find_container(options)
-        if options[:xpath].present?
-          self.container = page.first(:xpath, options[:xpath])
+        if options.has_key? :xpath
+          self.container = page.find(:xpath, options[:xpath])
+        elsif options.has_key? :css
+          self.container = page.find(:css, options[:css])
         else
-          label = page.first("label", text: options[:from]) || page.first(options[:from])
-          self.container = label.find(:xpath, '..').find(".select2-container")
+          select_name = options[:from]
+          self.container = page.find("label", text: select_name).find(:xpath, '..').find(".select2-container")
         end
       end
 
       def focus!
-        css_class = if multiselect then '.select2-choices' else '.select2-choice' end
-        container.find(css_class).click
-      end
-
-      def search!(value)
-        page.find(:xpath, "//body").find("input.select2-input").set(value)
-        page.execute_script(%|$("input.select2-input:visible").keyup();|)
+        if self.container.has_selector?(".select2-selection")
+          # select2 version 4.0
+          self.container.find(".select2-selection").click
+        elsif select2_container.has_selector?(".select2-choice")
+          self.container.find(".select2-choice").click
+        else
+          self.container.find(".select2-choices").click
+        end
       end
     end
   end
 end
 
-# This module gets included in the Cucumber or RSpec World
+# This module gets included in the Cucumber, RSpec, or MiniTest World
 module Capybara
   module Select2
     def select2(value, options = {})
-      raise "Must pass a hash containing 'from' or 'xpath'" unless options.is_a?(Hash) and [:from, :xpath].any? { |k| options.has_key? k }
+      raise "Must pass a hash containing 'from' or 'xpath' or 'css'" unless options.is_a?(Hash) and [:from, :xpath, :css].any? { |k| options.has_key? k }
+
       element = Capybara::Select2::Element.new(self, options)
-      element.initiate!(value)
-      element.select_match!
+      element.initiate!
+      element.select_match!(value)
     end
   end
 end
